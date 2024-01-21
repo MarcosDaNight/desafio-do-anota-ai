7b1678a6a091dbc2f4fd7ad355c20e445a0382ec
Marcos Guillermo <marcos.cosson@ccc.ufcg.edu.br>
2024-01-20 22:23:20 -0300

    minor-fix

--- src/main/java/com/marcosdanight/desafioanotaai/domain/category/Catageory.java
+++ src/main/java/com/marcosdanight/desafioanotaai/domain/category/Catageory.java
@@ -1,2 +1,24 @@
-package com.marcosdanight.desafioanotaai.domain.category;public class Catageory {
+package com.marcosdanight.desafioanotaai.domain.category;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Catageory {
+    @Id
+    private String id;
+
+    private String title;
+
+    private String description;
+
+    private String ownerId;
+
 }
