894313e5360b46697a6100b5aa96928e85d1cc2f
Marcos Guillermo <marcos.cosson@ccc.ufcg.edu.br>
2024-02-13 10:12:40 -0300

    feat: finishing aws sqs, lambda and s3 config

--- src/main/java/com/marcosdanight/desafioanotaai/domain/category/Category.java
+++ src/main/java/com/marcosdanight/desafioanotaai/domain/category/Category.java
@@ -4,6 +4,7 @@ package com.marcosdanight.desafioanotaai.domain.category;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.json.JSONObject;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -25,5 +26,17 @@ public class Category {
         this.title = categoryData.title();
         this.description = categoryData.description();
         this.ownerId = categoryData.ownerId();
+    }
+
+    @Override
+    public String toString() {
+        JSONObject json = new JSONObject();
+        json.put("title", this.title);
+        json.put("description", this.description);
+        json.put("ownerId", this.ownerId);
+        json.put("id", this.id);
+        json.put("type", "Category");
+
+        return json.toString();
     }
 }
